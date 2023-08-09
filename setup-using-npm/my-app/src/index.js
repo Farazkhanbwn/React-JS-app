fae82cf452b97b840c9014d1afc283b5ac9d010c
Faraz Khan <122208660+Farazkhanbwn@users.noreply.github.com>
2023-08-09 18:39:10 +0500

    complete javascript Exprssion

--- setup-using-npm/my-app/src/index.js
+++ setup-using-npm/my-app/src/index.js
@@ -2,6 +2,10 @@
 import ReactDOM from "react-dom";
 import Student from "./Student";
 import App from "./App";
+import Exression from "./Js_expression/js_expression";
+import Person from "./Js_expression/js_expression";
+import Value from "./Js_expression/JSX_children";
+import React from "react";
 // import el from "./App";
 
 // let el = <h1>Hello React</h1>;
@@ -15,3 +19,6 @@ import App from "./App";
 
 // Class Example
 ReactDOM.render(<App /> , document.getElementById("root"));
+ReactDOM.render(<Exression name="Numan" marks = {100 + 3}/> , document.getElementById("expression"));
+ReactDOM.render(<Value>Lahore</Value> , document.getElementById("testing"));
+// ReactDOM.render(<Person /> , document.getElementById("testing"));
