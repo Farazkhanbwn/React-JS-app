db87711dedc2c7e485bf1d4449ab51f7040aa211
Faraz Khan <122208660+Farazkhanbwn@users.noreply.github.com>
2023-08-13 12:41:08 +0500

    update my code

--- setup-using-npm/my-app/src/Student.js
+++ setup-using-npm/my-app/src/Student.js
@@ -15,11 +15,13 @@ export default class Student extends Component{
         console.log("Student - Constructor Called");
         this.state = {
             roll : 101,
+            showMarks: true
         }
     }
     clickHandle = () => {
         console.log("Button Clicked");
-        this.setState({roll : this.state.roll + 2});
+        this.setState({roll : this.state.roll + 2, showMarks: !this.state.showMarks}); 
+
     }
 
     render(){
@@ -27,9 +29,8 @@ export default class Student extends Component{
         return <Fragment>
             {/* <h1>Pakistan is the Largest country all over the world</h1> */}
         <h1>Kindly Give Your corrct Name {this.props.name}</h1>
-        <Marks roll = {this.state.roll}/>
+       {this.state.showMarks &&  <Marks roll = {this.state.roll}/>}
         <button onClick={this.clickHandle}>Change</button>
-        
         </Fragment>
     }
 }
