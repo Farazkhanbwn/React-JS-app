8ed1f7de1de8c8a73a5ccf7837dffc1e39ebe03e
Faraz Khan <122208660+Farazkhanbwn@users.noreply.github.com>
2023-08-15 14:36:25 +0500

    completed My Hooks Topic

--- setup-using-npm/my-app/src/App.js
+++ setup-using-npm/my-app/src/App.js
@@ -1,5 +1,8 @@
 import React , {Component , Fragment} from "react";
 import Student from "./Student";
+import Hook from "./hooks/hooks";
+import StateEffectPractice from "./hooks/useEffect";
+import PracticeHook from "./hooks/hooks-practice";
 
 class App extends Component{
     componentDidMount(){
@@ -31,6 +34,9 @@ class App extends Component{
         return <Fragment>
             {/* <h1>We are Learning at IUB</h1> */}
             <Student name = "Numan"></Student>
+            <Hook />
+            <StateEffectPractice />
+            <PracticeHook />
         </Fragment>
     }
 }
