465cef1843d9a40e7f721bf04e9bec16973b40ed
Faraz Khan <farazkhanaa2555@gmail.com>
2023-08-27 22:59:40 -0700

    update code

--- setup-using-npm/my-app/src/condtional-rendering/guest.jsx
+++ setup-using-npm/my-app/src/condtional-rendering/guest.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Guest extends Component {
   render() {
     return (
       <>
-      <h3>We are at Guest</h3>
-      <p>Hello Developers Community</p>
-      <button onClick={this.props.clickData}>Login</button>
-      <button>Sign Up</button>
+        <h3>We are at Guest testing</h3>
+        <p>Hello Developers Community</p>
+        <button onClick={this.props.clickData}>Login</button>
+        <button>Sign Up</button>
       </>
-    )
+    );
   }
 }
